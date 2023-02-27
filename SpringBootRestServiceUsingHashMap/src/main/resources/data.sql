8528bfc08203bc1163163045d61c2886151aa68e
Manimaran37 <109197578+Manimaran37@users.noreply.github.com>
2023-02-27 11:59:26 +0530

    Delete SpringBootRestServiceUsingHashMap directory

--- SpringBootRestServiceUsingHashMap/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-create table country(id int primary key, country_name varchar(50), capital varchar(50));
-insert into country values (1,'India','Delhi');
-insert into country values (2,'USA','New York');
